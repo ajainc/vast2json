651abadbcb6beb74c255d6dc7507f56e1fc12c3d
saito hirokazu <saito_hirokazu@cyberagent.co.jp>
2017-12-11 15:28:34 +0900

    imp

--- vast2json.js
+++ vast2json.js
@@ -15,7 +15,7 @@ var xml2js_param = {
 var WRAPPER_LIMIT = 5;
 var count = 1;
 var vast2json = {};
-
+var imp = [];
 
 vast2json.toJson = function(req,callback){
 
@@ -33,40 +33,59 @@ vast2json.parse = function(str,callback){
 
   var self = this;
   self.xmlparse(str, function(json){
-    if( ( json && 
-        json.VAST && 
-        json.VAST.Ad && 
-        json.VAST.Ad.Wrapper && 
-        json.VAST.Ad.Wrapper.VASTAdTagURI && 
-        json.VAST.Ad.Wrapper.VASTAdTagURI._value )
-        ||
-        ( json && 
-        json.VAST && 
-        json.VAST.Ad[0] && 
-        json.VAST.Ad[0].Wrapper && 
-        json.VAST.Ad[0].Wrapper.VASTAdTagURI && 
-        json.VAST.Ad[0].Wrapper.VASTAdTagURI._value )
-      ){
 
-      var url = '';
-      if(Array.isArray(json.VAST.Ad)){
-        url = json.VAST.Ad[0].Wrapper.VASTAdTagURI._value;
+    try {
+      if( ( json && 
+          json.VAST && 
+          json.VAST.Ad && 
+          json.VAST.Ad.Wrapper && 
+          json.VAST.Ad.Wrapper.VASTAdTagURI && 
+          json.VAST.Ad.Wrapper.VASTAdTagURI._value )
+          ||
+          ( json && 
+          json.VAST && 
+          json.VAST.Ad[0] && 
+          json.VAST.Ad[0].Wrapper && 
+          json.VAST.Ad[0].Wrapper.VASTAdTagURI && 
+          json.VAST.Ad[0].Wrapper.VASTAdTagURI._value )
+        ){
+
+        var url = '';
+        var parent = '';
+        if(Array.isArray(json.VAST.Ad)){
+          parent = json.VAST.Ad[0];
+        }else{
+          parent = json.VAST.Ad;
+        }
+        url = parent.Wrapper.VASTAdTagURI._value;
+        if(Array.isArray(parent.Wrapper.Impression)){
+          parent.Wrapper.Impression.forEach(function(element, index, array) {
+              imp.push(element);
+          });
+        }else{
+          imp.push(parent.Wrapper.Impression);
+        }
+
+        self.fetch(url, function(doc) {
+
+          if(count <= WRAPPER_LIMIT){
+            count++;
+            self.parse(doc,callback);
+          }else{
+            callback('too meny Wrapper');
+          }
+          
+        }, function(e) {callback(e)});
       }else{
-        url = json.VAST.Ad.Wrapper.VASTAdTagURI._value
+        imp = imp.map(function(element, index, array) {
+            return element._value;
+        });
+        json.imp = imp;
+        callback(json);
       }
 
-      self.fetch(url, function(doc) {
-
-        if(count <= WRAPPER_LIMIT){
-          count++;
-          self.parse(doc,callback);
-        }else{
-          callback('too meny Wrapper');
-        }
-        
-      }, function(e) {callback(e)});
-    }else{
-      callback(json);
+    } catch (e) {
+      callback(e)
     }
   }, function(e){callback(e)})
 }
@@ -99,6 +118,11 @@ vast2json.xmlparse = function(xml, ok, ng){
       ok(result);
     }
   });
+}
+
+vast2json.send = function(url){
+
+  (new Image()).src = url;
 }
 
 module.exports = vast2json;
